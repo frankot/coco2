b9aec4f7be905a7672dfaf61236c5b0ba40a7190
frankot <frankiantki@gmail.com>
2025-05-14 21:38:06 +0200

    build fix

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
   },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
+ 
   poweredByHeader: false,
   reactStrictMode: true,
   compress: true,
