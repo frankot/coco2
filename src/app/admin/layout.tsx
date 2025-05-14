684e6406ac770a5da3775428b3af69b9035e8276
frankot <frankiantki@gmail.com>
2025-05-14 23:17:36 +0200

    logo in admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,15 @@
 import { Nav, NavLink } from "@/components/NavAdmin";
 import { LogOut } from "./_components/LogOut";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <div className="fixed inset-0 flex items-center justify-center -z-10 opacity-10 pointer-events-none">
+        <Image src="/logo.png" alt="Logo" width={600} height={600} priority />
+      </div>
       <Nav>
         <NavLink href="/admin">Dashboard</NavLink>
         <NavLink href="/admin/products">Produkty</NavLink>
